d2d5d7492c0af5d3d8c0c6ee1c182a5238157012
Högni Gylfason <klumhru@gmail.com>
2016-03-13 09:52:30 +0000

    initial app

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Layout = () => <h1>It really really works!</h1>
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<Layout />, app)
